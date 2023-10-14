a63ab9e87a6f8aa0f62a06dec8536f9054558b84
Daniil <85786951+danifox-s@users.noreply.github.com>
2023-10-14 19:37:39 +0300

    Init commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Connection;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            // TODO Generate telegram report
+        });
+
+        // TODO Long request detection
+    }
+}
